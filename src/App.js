011781133e9c32e6295d7de87c72eb11a2d29dac
akash kushwaha <akashkushwaha76986@gmail.com>
2023-03-10 22:52:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+let name = "Akash3";
+
+function App() {
+  return (
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li> 
+    </nav>
+    <div className="container">
+      <h1>hello {name}</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem impedit corrupti cum nihil, excepturi corporis et neque qui? Dolorem aliquam ex nam veniam!</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod earum magni cumque neque aspernatur incidunt modi nihil quam nesciunt vitae consequuntur nostrum facilis ex sint, laboriosam, eos voluptates eum molestiae.</p>
+    </div>
+    </>
+  );
+  }
+
+export default App;
